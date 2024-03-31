5a418269d40b1e6024b4261a8ed77700e1c64db6
minimalpizza <157652776+professorDinwiddie6@users.noreply.github.com>
2024-03-30 22:17:07 -0700

    Updated JS

--- first/javascript/index.js
+++ first/javascript/index.js
@@ -1,8 +1,160 @@
-$(document).ready(function(){
-    $("#mobile-button").click(function(){
-        $("#mobile-menu").slideDown("slow");
-    });
-    $("#close-menu").click(function(){
-        $("#mobile-menu").slideUp("slow");
-    });
+// $(document).ready(function(){
+//     $("#mobile-button").click(function(){
+//         $("#mobile-menu").slideDown("slow");
+//     });
+//     $("#close-menu").click(function(){
+//         $("#mobile-menu").slideUp("slow");
+//     });
+// });
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+// const animations = ['#about-text', '#portfolio-text', '#experience-text'];
+
+// const anim = animations.forEach(el => {
+//     gsap.from(el, {
+//         scrollTrigger: {
+//             trigger: el,
+//             toggleActions: "play none none none",
+//             start: "20px 80%",
+//             markers: true,
+//         },
+//         y: 200,
+//         opacity: 0,
+//         duration: 2
+//     })
+// });
+
+gsap.from('#chosilog', {
+    scrollTrigger: {
+        trigger: '.food-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    y: 200,
+    opacity: 0,
+    duration: 2,
+    delay: .25,
 });
+gsap.from('#kaldereta', {
+    scrollTrigger: {
+        trigger: '.food-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    y: 200,
+    opacity: 0,
+    duration: 2,
+    delay: .5,
+});
+gsap.from('#chicharo', {
+    scrollTrigger: {
+        trigger: '.food-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    y: 200,
+    opacity: 0,
+    duration: 2,
+    delay: .75,
+});
+gsap.from('#custom', {
+    scrollTrigger: {
+        trigger: '.food-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    y: 200,
+    opacity: 0,
+    duration: 2,
+    delay: 1,
+});
+gsap.from('#blog-content-l', {
+    scrollTrigger: {
+        trigger: '.blog-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    xPercent: -100,
+    opacity: 0,
+    duration: 2,
+});
+gsap.from('#blog-content-r', {
+    scrollTrigger: {
+        trigger: '.blog-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    xPercent: 100,
+    duration: 2,
+    opacity: 0,
+});
+gsap.from('#blog-img-r', {
+    scrollTrigger: {
+        trigger: '.blog-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    xPercent: 100,
+    opacity: 0,
+    duration: 2,
+    delay: .25,
+});
+gsap.from('#blog-img-l', {
+    scrollTrigger: {
+        trigger: '.blog-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    xPercent: -100,
+    opacity: 0,
+    duration: 2,
+    delay: .25,
+});
+gsap.from('#review-1', {
+    scrollTrigger: {
+        trigger: '.testimonials-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    y: 200,
+    opacity: 0,
+    duration: 2,
+});
+gsap.from('#review-2', {
+    scrollTrigger: {
+        trigger: '.testimonials-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    y: 200,
+    opacity: 0,
+    duration: 2,
+    delay: .25,
+});
+gsap.from('#review-3', {
+    scrollTrigger: {
+        trigger: '.testimonials-container',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    y: 200,
+    opacity: 0,
+    duration: 2,
+    delay: .5,
+});
+gsap.from('#blue-section-text', {
+    scrollTrigger: {
+        trigger: '.blue-section',
+        toggleActions: "play none none none",
+        start: "top 80%",
+    },
+    y: 200,
+    opacity: 0,
+    duration: 2,
+    delay: .5,
+});
+
+
+
